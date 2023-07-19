e42191949a78b61a0b69cb27e2ab6f4c879fe4a4
Brendan Shelton <brendan.d.shelton@gmail.com>
2023-07-19 16:00:40 -0400

    auth models

---
+++ Dtos/UserForLoginDto.cs
@@ -0,0 +1,10 @@
+namespace DotnetAPI.Dtos
+{
+    partial class UserForLoginDto
+    {
+        string Email {get; set;} = "";
+        string Password {get; set;} = "";
+        string PasswordConfirm {get; set;} = "";
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+This project was made using the tutorial found here:
+https://www.udemy.com/course/net-core-with-ms-sql-beginner-to-expert/
